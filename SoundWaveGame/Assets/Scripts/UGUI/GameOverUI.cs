d6e2416b840bfe3bc72d696022e98b213c2761e7
Cain51 <84227597@163.com>
2025-11-01 13:44:05 +0800

    新增退出游戏按钮

--- SoundWaveGame/Assets/Scripts/UGUI/GameOverUI.cs
+++ SoundWaveGame/Assets/Scripts/UGUI/GameOverUI.cs
@@ -11,15 +11,31 @@ public class GameOverUI : MonoBehaviour
     GameObject _graphicRoot;
     [SerializeField]
     Button _retryButton;
+    [SerializeField]
+    Button _quitButton; // 新增的退出游戏按钮
 
     private void Awake()
     {
         Instance = this;
         _graphicRoot.SetActive(false);
         _retryButton.onClick.AddListener(() => GameManager.Instance.Retry());
+        // 为退出按钮添加点击事件
+        _quitButton.onClick.AddListener(() => QuitGame());
     }
     public void Show(bool cond)
     {
         _graphicRoot.SetActive(cond);
+    }
+    
+    // 退出游戏的方法
+    private void QuitGame()
+    {
+        #if UNITY_EDITOR
+            // 如果在Unity编辑器中运行，停止播放
+            UnityEditor.EditorApplication.isPlaying = false;
+        #else
+            // 如果在打包后的游戏中，退出应用程序
+            Application.Quit();
+        #endif
     }
 }

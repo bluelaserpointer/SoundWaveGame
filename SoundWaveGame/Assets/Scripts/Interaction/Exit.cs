ebff59515a90a70d7f0f961947da7af7ed5879a5
bluelaserpointer <bluelaserpointer@yahoo.co.jp>
2025-11-08 17:01:05 +0800

    fix: 第二关未显示撤离点

--- SoundWaveGame/Assets/Scripts/Interaction/Exit.cs
+++ SoundWaveGame/Assets/Scripts/Interaction/Exit.cs
@@ -2,25 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[DisallowMultipleComponent]
 public class Exit : MonoBehaviour
 {
     [SerializeField]
-    Transform _randomPositionCandidateRoot;
+    Transform _randomExitPositionCandidatesRoot;
     [SerializeField]
     PlayerEnterDetector area;
 
     private void Awake()
     {
         area.OnPlayerEnter.AddListener(OnPlayerEnter);
+        gameObject.SetActive(false);
     }
-    public void Show()
+    public void AppearInRandomPosition()
     {
         gameObject.SetActive(true);
         RandomizePosition();
     }
     public void RandomizePosition()
     {
-        Transform randomAnchor = _randomPositionCandidateRoot.GetChild(Random.Range(0, _randomPositionCandidateRoot.childCount - 1));
+        Transform randomAnchor = _randomExitPositionCandidatesRoot.GetChild(Random.Range(0, _randomExitPositionCandidatesRoot.childCount - 1));
         transform.SetPositionAndRotation(randomAnchor.position, randomAnchor.rotation);
     }
     public 
